b00eb3ee1facee05b717fe09529e9636d0fdc9ac
lesitoan <lesitoan.media@gmail.com>
2025-08-15 10:41:55 +0700

    init layout

--- src/views/LoginScreen/index.js
+++ src/views/LoginScreen/index.js
@@ -1,7 +1,9 @@
+import LoginForm from './components/LoginForm';
+
 export default function LoginScreen() {
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-green-500">
-      <h1 className="text-4xl font-bold">LOGIN SCREEN</h1>
+    <div className="flex items-center justify-center h-screen bg-green-500">
+      <LoginForm />
     </div>
   );
 }
